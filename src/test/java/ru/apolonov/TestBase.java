b09fd2c68dc2afde39a7ec2e10c32b279486912f
vasvap <vasvap@gmail.com>
2022-01-28 23:35:56 +0300

    change selenide version

--- src/test/java/ru/apolonov/TestBase.java
+++ src/test/java/ru/apolonov/TestBase.java
@@ -12,7 +12,8 @@ public class TestBase {
     @BeforeAll
     static void setup() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.browserSize = "1680x1050";
+        //Configuration.browserSize = "1680x1050";
+        Configuration.startMaximized = true;
 
         // запуск тестов на удаленном браузере Selenoid
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
